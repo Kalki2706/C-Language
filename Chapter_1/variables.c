b7a690ec407e19b8983330c921de778d5dd16b15
Kalki <kalkithewarrior2706@proton.me>
2023-02-20 21:31:57 +0530

    listed basic variables

---
+++ Chapter_1/variables.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 4;
+    // Int b = 8.5;
+    // Not recommended because 8.5 is not an integer
+
+    float b = 8.5;
+    char c = 'u';
+    return 0;
+}
